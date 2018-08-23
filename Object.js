40d05143125299faff3212cd91a698c2d4ef21c3
thisnameisprivate <2632798785@qq.com>
2018-08-23 16:57:21 +0800

    last commont

--- Object.js
+++ Object.js
@@ -88,3 +88,98 @@ Object.defineProperty(myObject, 'a', { // 设置属性描述符
 });
 console.log(myObject.a);
 console.log(Object.getOwnPropertyDescriptor(myObject, 'a'));
+// 不可写 case
+var object = {};
+Object.defineProperty(object, 'a', {
+    value: 2,
+    writable: false, // 不可写
+    enumerable: true,
+    configurable: true,
+});
+object.a = 3;
+console.log(object.a);
+delete object.a;
+console.log(object.a);
+var obj = {
+    a: 2,
+};
+Object.preventExtensions(obj); // 禁止对象扩展
+obj.b = 3;
+console.log(obj.b);
+var obj = {
+    a: 2,
+};
+var newObj = Object.seal(obj); // seal 就像在原有的对象上使用了,Object.preventExtensions();
+console.log(newObj);
+var obj = {
+    a: 3,
+};
+var newObj = Object.freeze(obj); // freeze 会冻结一个对象,就像是在原有的对象上使用了Object.seal();
+console.log(newObj);
+// [[ Get ]]
+// [[ Put ]]
+var obj = {
+    a: 2,
+};
+Object.defineProperty(obj, 'b', {
+    get: function () { // 给 b 设置一个getter
+        console.log(this.a * 2);
+        enumerable: true;
+    },
+});
+console.log(obj.a);
+console.log(obj.b);
+// 判断属性是否存在
+var obj = {
+a: 2,
+};
+console.log('a' in obj); // true
+console.log('b' in obj); // false
+// 可枚举性
+var myObject = {};
+Object.defineProperty(myObject, 'a', {
+    value: 3,
+    enumerable: true, // a 可枚举
+});
+Object.defineProperty(myObject, 'b', {
+    value: 2,
+    enumerable: false, // b 不可枚举
+});
+console.log(myObject.b); // 2
+console.log('b' in myObject); // true
+console.log(myObject.hasOwnProperty('b')); // true
+for (var k in myObject) {
+    console.log(k, myObject[k]);
+}
+// a 3
+var arrEnumerables = Object.keys(myObject); // 返回一个数组包含所有可以枚举的属性
+console.log(arrEnumerables);
+var allArr = Object.getOwnPropertyNames(myObject); // 返回一个数组包含所有属性(无论它们是否可枚举)
+console.log(allArr);
+var arr = [2, 3, 4];
+arr.forEach(a => { // 将数组中的元素依次传入function (a) {...} 作为参数
+    console.log(a);
+});
+arr.every(function (a) {
+    console.log(a);
+});
+var arr = [2, 3, 4];
+for (var v of arr) {  // for .. of 循环直接遍历数组的值
+    console.log(v);
+}
+// array 有内置的@@iterator来遍历数组
+// ps: 手动使用@@iterator遍历数组
+var myArray = [2, 3, 4, 5];
+var it = myArray[Symbol.iterator]();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+var myObject = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+for (var v of myObject) {
+    console.log(v);
+}
