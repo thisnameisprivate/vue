8ace3a8e38ffc22b16a67671b24df50cef9b51aa
thisnameisprivate <2632798785@qq.com>
2018-08-23 14:23:09 +0800

    You Don't Know JavaScript(top)-->objcet noteBook

--- Object.js
+++ Object.js
@@ -11,3 +11,80 @@ console.log(strPrimitive.length); // 13
 console.log(strPrimitive.charAt(3)); // m
 var number = 42.359;
 console.log(number.toFixed(2)); // 42.36
+// 可计算属性名
+var prefix = 'foo';
+var myObject = {
+    [prefix + 'bar'] : "hello",
+    [prefix + 'baz'] : "world"
+};
+console.log(myObject["foobar"]);
+console.log(myObject["foobaz"]);
+// 对象函数访问
+function foo () {
+    console.log('foo');
+}
+var somefoo = foo; // 对foo函数的引用
+var myObjcet = {
+    somefoo: foo,
+};
+console.log(foo);
+console.log(somefoo);
+console.log(myObjcet.somefoo);
+
+var myObject = {
+    foo: function () {
+        console.log('foo');
+    }
+};
+var someFoo = myObject.foo;
+console.log(someFoo);
+console.log(myObject.foo);
+// 数组
+var myArray = ['foo', 32, 'bar'];
+myArray.baz = 'baz'; // 以对象方式添加的元素不会影响数组的长度
+console.log(myArray.length); // 3
+console.log(myArray.baz);  // baz
+// 看起来像数字的下表会被自动转换成数字
+var myArray = ['foo', 32, 'bar'];
+myArray['3'] = 'baz'; // 以数组方式添加的元素会影响数组的长度
+console.log(myArray.length); // 4
+console.log(myArray[3]); // baz
+// 复制对象
+function anotherFunction () {
+
+}
+var anotherObjcet = {
+    c: true,
+};
+var anotherArray = [];
+var myObjcet = {
+    a: 2,
+    b: anotherObjcet,
+    c: anotherArray,
+    d: anotherFunction,
+};
+anotherArray.push(anotherObjcet, myObjcet);
+// JSON 字符串复制并解析出一个结构和值完全一样的对象
+// var newObj = JSON.parse(JSON.stringify(someobj));
+// ES6 浅复制 case syntax ..
+// var newObj = Objcet.assign(targetObjcet, source, source2, source3);
+var newObj = Object.assign({}, myObjcet);
+console.log(newObj.a);
+console.log(newObj.b === myObjcet.b);
+console.log(newObj.c === myObjcet.c);
+console.log(newObj.d === myObjcet.d);
+// getOwnPropertDescriptor 获取对象的数据描述符. writable(可写), enumerable(可枚举), configurable(可配置);
+var object = {
+    a: 2,
+};
+console.log(Object.getOwnPropertyDescriptor(object, 'a'));
+// ps:
+var myObject = {};
+Object.defineProperty(myObject, 'a', { // 设置属性描述符
+    value: 2,
+    writable: true,
+    enumerable: true,
+    configurable: true,
+});
+console.log(myObject.a);
+console.log(Object.getOwnPropertyDescriptor(myObject, 'a'));
