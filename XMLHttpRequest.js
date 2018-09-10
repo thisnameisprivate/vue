0747515ea30ac88b181ebb6baa37802cee359d98
thisnameisprivate <2632798785@qq.com>
2018-09-10 14:41:39 +0800

    删除多余code

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -3,7 +3,6 @@ Request.open("GET", 'xxx-xxx-xxx');
 Request.send();
 Request.onreadystatechange = function () {
     if (Request.readyState = 4 && Request.status == 200) {
-       // document.getElementsByClassName('div')[0].contentWindow.body.innerHTML = Request.responseText;
         document.getELementsBYClassName('div')[0].contentWindow.document.body.innerHTML = Request.responseText;
     }
 }
