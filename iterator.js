470d53997ebb6425b4c3b768f7a5f842c3f34097
thisnameisprivate <2632798785@qq.com>
2018-09-18 09:50:32 +0800

    iterable(可迭代)
    使用生成器实现无线数字序列生产者

--- iterator.js
+++ iterator.js
@@ -26,3 +26,37 @@ var something = ( () => {
         }
     }
 })();
+// for .. of
+for (var v of something) {
+    console.log(v);
+    if (v > 500) {
+        break;
+    }
+}
+// Array for .. of
+var a = [1, 2, 3, 4, 5];
+for (var v of a) {
+    console.log(v);
+}
+// 1, 2, 3, 4, 5
+var a = [1, 2, 3, 4, 5];
+var it = a [Symbol.iterator]();
+it.next().value;
+it.next().value;
+it.next().value;
+// 生成器
+function *something () {
+    var nextVal;
+    while (true) {
+        if (nextVal == undefined) {
+            nextVal = 1;
+        } else {
+            nextVal= (3 * nextVal) + 6;
+        }
+        yield nextVal;
+    }
+}
+var it = something();
+it.next().value; // 1
+it.next().value; // 9
+it.next().value; // 33
